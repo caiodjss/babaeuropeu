9677686110d2625a51a281eae166ddb5fa8d466a
caiodjss <cdj@outlook.com.br>
2025-04-17 19:18:22 -0300

    organizando as pastas

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
-  const img = document.querySelector("#profile img")
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  const img = document.querySelector("#profile img");
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "/src/assets/avatar-light.png");
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "/src/assets/avatar.png");
   }
 }
+
 
 function mostrarTexto() {
   const container = document.querySelector(".login-container");
